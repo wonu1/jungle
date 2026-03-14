52793e23a3b0d11f904a088ba61db9004a2cf9ce
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-14 14:14:34 +0900

    feat(base): week2 09_insert_sort 구현

--- week2/basic/09_insertion_sort.py
+++ week2/basic/09_insertion_sort.py
@@ -42,6 +42,15 @@ def insertion_sort(arr):
     ## arr[j] > key인 동안 원소를 오른쪽으로 이동
     ## 찾은 위치에 key 삽입
     pass
+    for i in range(1,n):
+        key = arr[i]
+        j = i - 1
+        while(j>=0 and arr[j]>key):
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+                    
+
     
     return arr
 
@@ -61,8 +70,10 @@ def insertion_sort_with_steps(arr):
         
         # TODO: 삽입 위치 찾기 및 이동
         pass
-        
-        arr[j + 1] = key
+        while(j>=0 and arr[j]>key):
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
         print(f"삽입 후: {arr}")
     
     return arr
