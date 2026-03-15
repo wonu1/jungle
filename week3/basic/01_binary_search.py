cd833a0132beebb2cc4960deabfd3f5af266f427
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-15 23:28:07 +0900

    feat(base): week3 01_binary_search 구현

--- week3/basic/01_binary_search.py
+++ week3/basic/01_binary_search.py
@@ -42,6 +42,15 @@ def binary_search(arr, target):
     ## 같으면 mid 반환
     ## target이 더 크면 left = mid + 1
     ## target이 더 작으면 right = mid - 1
+    while left <= right:
+        mid = int((left + right) /2)
+        if (arr[mid] == target):
+            return mid
+        elif (arr[mid] < target):
+            left = mid +1
+        else:
+            right = mid -1
+
     pass
     
     return -1
