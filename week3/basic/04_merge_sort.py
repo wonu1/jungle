c2b5edd0ffe71d7f623695c050c74b8f01d8ac8d
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-15 23:31:13 +0900

    feat(base): 04_merge_sort 구현

--- week3/basic/04_merge_sort.py
+++ week3/basic/04_merge_sort.py
@@ -34,6 +34,9 @@ def merge(arr, left, mid, right):
     """
     # TODO: 왼쪽과 오른쪽 부분 배열을 임시 배열로 복사
     pass
+    left_arr = arr[left : mid + 1]
+    right_arr = arr[mid + 1 : right + 1]
+
     
     # TODO: 두 배열을 병합
     pass
@@ -41,10 +44,32 @@ def merge(arr, left, mid, right):
     
     # TODO: left_arr와 right_arr를 비교하며 작은 값을 arr에 복사
     pass
-    
+    i = 0
+    j = 0
+    k = left
+    while  i < len(left_arr) and j < len(right_arr) :
+        if left_arr[i] < right_arr[j]:
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        else:
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
     # TODO: 남은 원소들을 복사
     # left_arr에 남은 원소가 있으면 복사
     # right_arr에 남은 원소가 있으면 복사
+    while  i < len(left_arr) :
+        arr[k] = left_arr[i]
+        i += 1
+        k += 1
+
+    while j < len(right_arr) :
+        arr[k] = right_arr[j]
+        j += 1
+        k += 1
+
     pass
 
 def merge_sort_helper(arr, left, right):
@@ -61,6 +86,13 @@ def merge_sort_helper(arr, left, right):
     ## 왼쪽 절반 재귀 정렬
     ## 오른쪽 절반 재귀 정렬
     ## 정렬된 두 절반을 병합
+
+    if left< right:
+        mid = (left + right) // 2
+        merge_sort_helper(arr, left, mid)
+        merge_sort_helper(arr, mid +1, right)
+        merge(arr, left, mid, right)
+
     pass
 
 def merge_sort(arr):
