88fe5f42501b599e4d4334ff37e797e29ac34b95
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-16 23:47:54 +0900

    feat(boj): 2675 문자열 풀이 추가

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,11 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+T = int(input())
+
+for i in range(T):
+    num, str = input().split()
+    for j in list(str):
+        for k in range(int(num)):
+            print(j,end="")
+    print()
