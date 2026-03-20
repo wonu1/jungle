3834a57b44ab1ad82360870e7f194ad77f3b6bc4
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-20 20:17:17 +0900

    feat(boj): 1978 정수론 소수 찾기 풀이

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,20 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+
+import math
+
+N = int(input())
+nums = list(map(int, input().split()))
+count = 0
+if 1 in nums:
+    count += 1
+
+for i in nums:
+    for j in range(2,int(math.sqrt(i))+1):
+        if (i % j == 0 and i !=2 and i != 3):
+            count += 1
+            break
+
+
+print(N-count)
