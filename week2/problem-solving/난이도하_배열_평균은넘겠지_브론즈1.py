2889054045cd5bc154c18646a7cc86ab06866979
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-20 20:16:16 +0900

    feat(boj): 4344 배열 문제 풀이

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,25 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+    
+nums = []
+C = int(input())
+for i in range(C):
+    sum = 0
+    average = 0
+    success = 0
+    
+    nums = list(map(int, input().split()))
+
+    for j in range(1, len(nums)):
+        sum += nums[j]
+
+    average = sum / nums[0]
+
+    for j in nums[1:]:
+        if(j > average):
+            success +=1
+    
+    result = success / nums[0] * 100
+
+    print(f"{result:.3f}"+"%")
