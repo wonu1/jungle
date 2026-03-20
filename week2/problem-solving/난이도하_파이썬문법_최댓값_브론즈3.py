3b3d6d22edcfc432ca92eac962ffd3e37e94e82a
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-20 20:18:31 +0900

    feat(boj): 2562 최댓값 풀이

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,9 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+num = []
+for i in range(9):
+    num.append(int(input()))
+
+print(max(num))
+print(num.index(max(num))+1)
