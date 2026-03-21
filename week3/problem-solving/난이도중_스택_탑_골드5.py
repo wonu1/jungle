7135f2f2b38d2457988b9472c593caf1af054aed
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-21 20:52:14 +0900

    chore(boj): 2493 탑 구현 진행

--- week3/problem-solving/난이도중_스택_탑_골드5.py
+++ week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,23 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+N = int(input())
+arr = list(map(int, input().split()))
+print(arr)
+stack = []
+result = []
+
+for i in range(N):
+    stack.append(arr[i])
+    test = stack.pop()
+    print('test: ', test)
+
+    for j in range(0, i):
+        print('arr[i]:',arr[i], 'j:',j,'i:', i)
+        # if stack[j] > arr[i]:
+        #     result.append(j+1)
+        #     break
+    else:
+        result.append(0)
+    
+print(result)
