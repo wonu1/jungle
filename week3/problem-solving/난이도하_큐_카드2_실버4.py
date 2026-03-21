189cacd76589a28655047f4de2bc63486258692a
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-21 22:11:37 +0900

    feat(boj): 2164 카드2 문제 풀이

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,16 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+N = int(input())
+
+queue = deque(range(1, N + 1))
+
+while len(queue) > 1:
+    queue.popleft()
+    num = queue.popleft()
+    queue.append(num)
+
+print(queue[0])
+    
