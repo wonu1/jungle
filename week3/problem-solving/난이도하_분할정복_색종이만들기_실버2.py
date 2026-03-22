9114ab27f47906ba0148335c5e3a6de0235acf5e
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-22 20:44:09 +0900

    chore(boj): 2630 색종이 만들기 구현 진행

--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,15 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+
+N = int(input())
+nums = []
+for i in range(N):
+    nums.append(list(map(int, input().split())))
+    print(nums[i])
+
+mid = int((len(nums[0])+1) / 2)
+print(mid)
+
+for i in range(mid):
+    print(nums[i][0 : mid])
+    
