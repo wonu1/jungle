98c684da6cdcc00abf8fd12698ce2eeef096689d
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-23 21:03:16 +0900

    chore(boj): 2295 세 수의 합 구현 진행

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,17 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+N = int(input())
+result = []
+num = []
+
+for i in range(N):
+    num.append(int(input()))
+
+for x in range(len(num)):
+    for y in range(x, len(num)):
+        for z in range(y, len(num)):
+            if (num[x] + num[y] + num[z]) in num:
+                result.append(num[x] + num[y] + num[z])
+
+print(result[-1])
