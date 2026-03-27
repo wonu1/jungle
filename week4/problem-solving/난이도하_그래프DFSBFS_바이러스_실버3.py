15fd6eea8ae8c9dae39c0375bc7539ffa75c9036
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-27 22:09:18 +0900

    feat(boj): 2606 바이러스 문제 풀이

--- week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,28 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+from collections import deque
+
+
+computer = int(input())
+edge = int(input())
+edges = []
+
+for i in range(edge):
+    edges.append(list(input().split()))
+
+queue = deque('1')
+visited = set()
+
+while queue:
+    current = queue.popleft()
+    visited.add(current)
+
+    for u, v in edges:
+        if v == current and u not in visited:
+            queue.append(u)
+        if u == current and v not in visited:
+            queue.append(v)
+
+print(len(visited)-1)
+
+
