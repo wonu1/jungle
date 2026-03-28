b53763a7dada0c570eaca4f13538796cfe5359b1
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-28 21:58:03 +0900

    feat(boj): 9372 상근이의 여행 문제 풀이

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,12 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, (input().split()))
+
+    for _ in range(M):
+        input()
+
+    print(N-1)
