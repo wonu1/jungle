5c3651b4c3eb3e745683fefafcfd10fd1f1e3c5b
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-03-28 20:41:29 +0900

    chore(boj): 16173 점프왕 쩰리 구현 진행

--- week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
+++ week4/problem-solving/난이도하_그래프DFSBFS_점프왕쩰리_실버4.py
@@ -1,2 +1,29 @@
 # 그래프, DFS, BFS - 점프왕 쩰리 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/16173
+import sys
+sys.setrecursionlimit(10**5)
+
+N = int(input())
+
+arr = [] * N
+for _ in range(N):
+    arr.append(list(map(int,(input().split()))))
+
+
+def jump(i, j):
+    if i >= N or j >= N:
+        return False
+    
+    if i == N-1 and j == N-1:
+        return True
+    
+    current = arr[i][j]
+
+    return jump(i + current, j) or jump(i, j+ current)
+    
+
+
+if jump(0, 0):
+    print('HaruHaru')
+else:
+    print('Hing')
