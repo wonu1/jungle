70b67581eeabead6bdbe2686833f9fccde91707f
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-04 14:16:28 +0900

    feat(base): week5 01 dp fibonacci 구현

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -85,15 +85,22 @@ def fibonacci_memo(n, memo=None):
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
     pass
+    if memo is None:
+        memo = {}
     
     # TODO: base case 
     pass
+    if n == 0 or n ==1:
+        return n
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
     pass
+    if n in memo.keys():
+        return memo[n]
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
     pass
+    memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
     
     return memo[n]
 
