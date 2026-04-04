8adfa93afa4d99b6630815921bf5cd82d644fd4e
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-04 22:07:39 +0900

    feat(boj): 2748 피보나치 수 2 문제 풀이

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,20 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+N = int (input())
+
+def fibo(n, memo = None):
+
+    if n == 1 or n == 0:
+        return n
+
+    if memo is None:
+        memo = {}
+
+    for i in range(n+1):
+        if i not in memo.keys():
+            memo[i] = fibo(n-1, memo) + fibo(n-2, memo)
+
+    return memo[n]
+
+print(fibo(N))
