9e2a8b86ca212cf3702d5efe15f4780725ad2791
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-04 20:48:11 +0900

    feat(base): week5 02 dp stairs 구현

--- week5/basic/02_dp_stairs.py
+++ week5/basic/02_dp_stairs.py
@@ -47,13 +47,19 @@ def climb_stairs(n):
     """
     # TODO: 특별한 경우 처리
     pass
-    
+    if n == 0 or n == 1 or n == 2:
+        return n
     
     # TODO: dp 배열 생성 및 초기화
     pass
-    
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+
     # TODO: 작은 문제부터 차례로 계산
     pass
+    for i in range(3,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
     
     return dp[n]
 
