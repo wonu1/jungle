6c975edbcf7e5e505d29699d2b3fe776a771ff33
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-05 23:49:13 +0900

    feat(boj): 9933 해시테이블 민균이의 비밀번호 문제 풀이

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,18 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+N = int(input())
+str_list = []
+found = False
+for i in range(N):
+    str = input()
+    str_list.append(str)
+
+for i in str_list:
+    for j in str_list:
+        if i == j[::-1]:
+            print(len(i), i[len(i)//2])
+            found = True
+            break
+    if(found == True):
+        break
