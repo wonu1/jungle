4faf0891f94c4037c518d9006b160bbd5659e5ce
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-05 23:39:14 +0900

    feat(boj): 분할정복 곱셈 문제 풀이

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,41 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+A, B, C = map(int, input().split())
+
+def power(A, B):
+    if B == 1:
+        return A % C
+    mid = B // 2
+    temp = power(A, mid)
+    
+    if B % 2 == 0:
+        return  temp * temp % C
+    else:
+        return temp * temp * A % C
+    
+result = power(A,B)
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# pow(A, B)
+# for i in range(B):
+#     A *= A
+#     A %= C
+
+# print( A % C )
