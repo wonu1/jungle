f32dbb48fc3014149128ebfc2458af834dd4c453
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-05 23:44:46 +0900

    feat(boj): 2470 이분탐색 두 용액 문제 풀이

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,45 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+N = int(input())
+
+nums = list(map(int,input().split()))
+nums.sort()
+result = []
+min = 2000000000
+
+for i in range(len(nums)):
+    target = -(nums[i])
+
+    left = i+1
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right)//2
+
+        if target == nums[mid]:
+            result.append(nums[i])
+            result.append(nums[mid])
+            break
+        elif target > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    else:
+        if left < len(nums):
+            result.append(nums[i])
+            result.append(nums[left])
+        if right > i:
+            result.append(nums[i])
+            result.append(nums[right])
+        
+
+for i in range(0,len(result),2):
+    if min > abs(result[i] + result[i+1]):
+        min = abs(result[i] + result[i+1])
+        end = i
+
+if result[end]< result[end+1]:
+    print(result[end], result[end+1])
+else:
+    print(result[end+1], result[end])
