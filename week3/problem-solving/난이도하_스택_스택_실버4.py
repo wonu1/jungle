3a916e3f297c84751db64c59ddac197c7cb75cdc
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-05 23:47:21 +0900

    feat(boj): 10828 스택 문제 풀이

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,33 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+result = []
+
+for _ in range(N):
+    arr = input().split()
+    command = arr[0]
+
+    if command == 'push':
+        stack.append(arr[1])
+    elif command == 'pop':
+        if stack:
+            result.append(stack.pop())
+        else:
+            result.append('-1')
+    elif command == 'size':
+        result.append(str(len(stack)))
+    elif command == 'empty':
+        result.append('0' if stack else '1')
+    elif command == 'top':
+        if stack:
+            result.append(stack[-1])
+        else:
+            result.append('-1')
+
+print('\n'.join(result))
