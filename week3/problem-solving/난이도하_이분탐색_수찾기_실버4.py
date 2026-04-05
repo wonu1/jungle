0d2e5c0cd5dfc9e11dd07a7a731daca18d9c45f5
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-05 23:48:23 +0900

    feat(boj): 1920 이분탐색 수 찾기 문제 풀이

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,26 @@
-# 이분탐색 - 수 찾기 (백준 실버4)
-# 문제 링크: https://www.acmicpc.net/problem/1920
+N = int(input())
+number_list = list(map(int, input().split()))
+M = int(input())
+target_list = list(map(int, input().split()))
+
+number_list.sort()
+result = []
+
+for target in target_list:
+    left = 0
+    right = len(number_list) - 1
+
+    while left<=right:
+        mid = (left + right)//2
+
+        if target == number_list[mid]:
+            result.append('1')
+            break
+        elif target < number_list[mid]:
+            right = mid -1
+        else :
+            left = mid +1
+    else:
+        result.append('0')
+
+print('\n'.join(result))
