303d69500fedf46303a7f6b63dec0c0cba09ace7
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-07 22:36:18 +0900

    feat(boj): 1541 잃어버린 괄호 문제 풀이

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,46 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+# import re
+
+# sic = input()
+
+# flag = False
+# result = []
+
+# for i in sic:
+#     if i == '-':
+#         if flag == False:
+#             result.append('-')
+#             result.append('(')
+#             flag = True
+#         else:
+#             result.append(')')
+#             result.append('-')
+#             result.append('(')
+#             flag = True
+#     elif i.isdecimal():
+#         result.append(i)
+#     elif i == '+':
+#         result.append(i)
+
+# if flag:
+#     result.append(')')
+
+# result = ''.join(result)
+# normalized = re.sub(r'\d+', lambda m: str(int(m.group())), result)
+
+# result = eval(normalized)
+
+# print(result)
+
+
+sic = input()
+
+s = (sic.split('-'))
+
+result = sum(map(int,(s[0].split('+'))))
+
+for i in s[1:]:
+    result -= sum(map(int,(i.split('+'))))
+
+print(result)
