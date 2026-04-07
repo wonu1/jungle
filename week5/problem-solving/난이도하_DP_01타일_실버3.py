bb3f8a57b3a7756455fdc8492f9ed8cb7d98a91f
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-07 19:52:33 +0900

    feat(boj): 1904 01타일 문제 풀이

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,21 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+N = int(input())
+
+def fibo(n, memo = None):
+
+    if n == 1 :
+        return 1
+    
+    if memo is None:
+        memo = [0] * (n+1)
+        memo[0] = 1
+        memo[1] = 1
+
+    for i in range(2,n+1):
+        memo[i] = (memo[i-1]% 15746 + memo[i-2]% 15746)% 15746
+    
+    return memo[n]
+
+print(fibo(N))
