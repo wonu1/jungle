037571ab3e8c5820453ac2c2cd5ee2c58760edb4
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-07 21:17:42 +0900

    feat(boj): 12865 평범한 배낭 문제 풀이

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,18 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+
+N , limit = map(int,(input().split()))
+
+nums = []
+
+for i in range(N):
+    nums.append(tuple(map(int,(input().split()))))
+
+dp = [0] * (limit+1)
+
+for w, v in nums:
+    for j in range(limit,w - 1,-1):
+        dp[j] = max(dp[j] , dp[j - w] + v)
+
+print(dp[limit])
+
