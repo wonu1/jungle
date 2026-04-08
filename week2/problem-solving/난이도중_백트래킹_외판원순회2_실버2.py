7fc96bcd81de6aacecc37fe749db225fdfe97df8
wonu1 <54071790+wonu1@users.noreply.github.com>
2026-04-08 20:43:11 +0900

    chore(boj): 10971 외판원 순회 2 구현 진행

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,40 @@
 # 백트래킹 - 외판원 순회 2 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10971
+
+
+N = int(input())
+cost = [0] * N
+
+for i in range(N):
+    cost[i] = list(map(int,input().split()))
+
+result = 999
+
+def travel(route):
+    global result
+    if(len(route) == N):
+        if(result > sum(route)):
+            result = sum(route)
+            print(result)
+        return
+
+    for i in cost:
+        for j in i:
+            if(j == 0 or j in route):
+                continue
+            route.append(j)
+            print("route:",route)
+            travel(route)
+            route.pop()
+
+travel([])
+
+print(cost)
+"""
+4
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0
+"""
+
